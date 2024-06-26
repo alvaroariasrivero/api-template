4228e5e9f8081631734a54054db2388557bb1607
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-06-26 18:08:18 +0200

    search book by fragment

--- controllers/bookApi.js
+++ controllers/bookApi.js
@@ -2,12 +2,26 @@ const Book = require('../models/book');
 const Author = require('../models/author');
 
 const getAllBooks = async (req, res) => {
-    try {
-        const data = await Book.find({}, '-_id').populate('id_autor', '-_id');
-        res.status(200).json(data);
-    } catch (error) {
-        console.error("Error en getAllBooks:", error);
-        res.status(400).json({ error: error.message });
+    const { titleFragment } = req.query;
+    if(titleFragment){
+        try {
+            const data = await Book.find({ titulo: { "$regex": titleFragment, "$options": "i" } }, '-_id').sort({ titulo: 'asc' }).populate('id_autor', '-_id');
+            if (!data) {
+                return res.status(404).json({ error: "Ningún parámetro coincide con la búsqueda" });
+            }
+            res.status(200).json(data);
+        } catch (error) {
+            console.error("Error en getAllBooks:", error);
+            res.status(400).json({ error: error.message });
+        }
+    }else{
+        try {
+            const data = await Book.find({}, '-_id').sort({ titulo: 'asc' }).populate('id_autor', '-_id');
+            res.status(200).json(data);
+        } catch (error) {
+            console.error("Error en getAllBooks:", error);
+            res.status(400).json({ error: error.message });
+        }
     }
 };
 
@@ -29,7 +43,7 @@ const getBooksByAuthorLastName = async (req, res) => {
 const getBooksByTitle = async(req, res) => {
     const { title } = req.query;
     try {
-        const data = await Book.findOne({ titulo: { '$regex' : `^${title}$`, $options: 'i'} }, '-_id').populate('id_autor', '-_id');
+        const data = await Book.findOne({ titulo: { '$regex' : `^${title}$`, '$options': 'i'} }, '-_id').populate('id_autor', '-_id');
         if(!data){
             return res.status(404).json({ error: "Libro no encontrado" });
         }
