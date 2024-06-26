758bb2be6450a404b2d11dc297d558280c9d51cd
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-06-26 12:41:31 +0200

    getbooks by author

--- controllers/bookApi.js
+++ controllers/bookApi.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
 
 const getAllBooks = async (req, res) => {
     try {
@@ -10,6 +11,22 @@ const getAllBooks = async (req, res) => {
     }
 };
 
+const getBooksByAuthorLastName = async (req, res) => {
+    const { lastName } = req.query;
+    try {
+        const author = await Author.findOne({ apellidos: lastName });
+        if (!author) {
+            return res.status(404).json({ error: "Autor no encontrado" });
+        }
+        const data = await Book.find({ id_autor: author._id }, '-_id').sort({ titulo: 'asc' }).populate('id_autor', '-_id nombre apellidos');
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error en getBooksByAuthorLastName:", error);
+        res.status(400).json({ error: error.message });
+    }
+};
+
 module.exports = {
-    getAllBooks
+    getAllBooks,
+    getBooksByAuthorLastName
 };
--- routes/booksApi.js
+++ routes/booksApi.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const bookApi = require('../controllers/bookApi');
 
 routes.get('/books', bookApi.getAllBooks);
+routes.get('/booksByAuthor', bookApi.getBooksByAuthorLastName);
 
 module.exports = routes;
